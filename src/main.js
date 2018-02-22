60a2c5720dee7ddc84a67ee737d9862dc9db8204
Edgar Talledos <onimenotsuki@gmail.com>
2018-02-21 18:15:41 -0600

    Add linters and update dependencies

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+import 'uikit/dist/css/uikit.min.css';
+import 'airbnb-browser-shims';
+
+// Import UIkit framework
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
 import './assets/stylesheets/main.scss';
+
+UIkit.use(Icons);
